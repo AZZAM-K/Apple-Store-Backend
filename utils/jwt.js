30f531e13b652b64124493b1b6348a04d0292acf
Azzam Al Kahil <azzam.alkahil@icloud.com>
2025-07-09 17:56:19 +0300

    fix 3

--- utils/jwt.js
+++ utils/jwt.js
@@ -8,7 +8,7 @@ const generateToken = (user, res) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: true,
-    sameSite: 'none',
+    sameSite: 'None',
     maxAge: 24 * 60 * 60 * 1000,
   })
   return token
